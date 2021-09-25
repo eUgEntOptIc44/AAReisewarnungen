f9d0714d844ad5a8fa889ea1a459285647814bea
Jean-Luc Tibaux <eUgEntOptIc@outlook.de>
2021-09-25 15:50:47 +0000

    trying to fix padding and alignment in slivers widget

--- lib/presentation/screens/user_details_page.dart
+++ lib/presentation/screens/user_details_page.dart
@@ -63,142 +63,175 @@ class UserDetailsPage extends StatelessWidget {
             SliverGrid(
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: Utils.getDeviceType() == 'phone' ? 1 : 2),
               delegate: SliverChildListDelegate([
-                Container(
-                  padding: const EdgeInsets.all(5),
-                  child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-                    Center(
-                      child: Hero(
-                        tag: user.id,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.all(Radius.circular(6.0)),
-                          child: Container(
-                            child: Align(
-                              alignment: Alignment.center,
-                              widthFactor: 0.50,
-                              heightFactor: 0.50,
-                              child: Image.network(user.flagUrl),
+                Align(
+                    alignment: Alignment.topLeft,
+                    child: Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.blue
+                      )
+                    ),
+                    child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+                      Center(
+                        child: Hero(
+                          tag: user.id,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                            child: Container(
+                              child: Align(
+                                alignment: Alignment.center,
+                                widthFactor: 0.50,
+                                heightFactor: 0.50,
+                                child: Image.network(user.flagUrl),
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 22.0,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(7.0),
-                      child: Text(
-                        user.title,
-                        style: TextStyle(
-                          fontSize: 25.0,
-                          fontWeight: FontWeight.w700,
-                        ),
-                        textAlign: TextAlign.left,
+                      SizedBox(
+                        height: 22.0,
                       ),
-                    ),
-                    SizedBox(
-                      height: 12.0,
-                    ),
-                  ]),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(5),
-                  child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-                    Padding(
-                      padding: EdgeInsets.all(7.0),
-                      child: StepProgressIndicator(
-                        totalSteps: 3,
-                        currentStep: UserWarningLevel(user),
-                        size: 30,
-                        selectedColor: Colors.orange,
-                        unselectedColor: Colors.lightGreen, //[200],
-                        customStep: (index, colorTemp, _) => colorTemp == Colors.orange
-                            ? Container(
-                                color: colorTemp,
-                                child: Tooltip(
-                                  message: warningLevelString(index),
-                                  child: Icon(
-                                    Icons.warning,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                              )
-                            : Container(
-                                color: colorTemp,
-                                child: Tooltip(
-                                  message: warningLevelString(index),
-                                  child: Icon(
-                                    Icons.remove,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                              ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 12.0,
-                    ),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Padding(
+                      Padding(
                         padding: EdgeInsets.all(7.0),
-                        child: Row(children: [
-                          Icon(
-                            Icons.update,
-                            //color: Colors.black,
-                            size: 18.0,
-                            semanticLabel: 'Letzte Änderung',
+                        child: Text(
+                          user.title,
+                          style: TextStyle(
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.w700,
                           ),
-                          Tooltip(
-                            message: "letzte Änderung: " + DateFormat('dd.MM.yyyy Hm').format(DateTime.fromMillisecondsSinceEpoch(user.lastModified * 1000)),
-                            child: Text(
-                              " " + timeago.format(DateTime.fromMillisecondsSinceEpoch(user.lastModified * 1000), locale: 'de'), // source: https://stackoverflow.com/a/50632411 - users: Mahesh Jamdade & Alex Haslam - License: CC BY-SA 4.0
-                              style: TextStyle(
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.w500,
-                              ),
-                              textAlign: TextAlign.left,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 5.0,
-                          ),
-                          Icon(
-                            Icons.update,
-                            //color: Colors.black,
-                            size: 18.0,
-                            semanticLabel: 'effective',
-                          ),
-                          Tooltip(
-                            message: "effective -> " + DateFormat('dd.MM.yyyy Hm').format(DateTime.fromMillisecondsSinceEpoch(user.effective * 1000)),
-                            child: Text(
-                              " " + timeago.format(DateTime.fromMillisecondsSinceEpoch(user.effective * 1000), locale: 'de'), // source: https://stackoverflow.com/a/50632411 - users: Mahesh Jamdade & Alex Haslam - License: CC BY-SA 4.0
-                              style: TextStyle(
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.w500,
-                              ),
-                              textAlign: TextAlign.left,
-                            ),
-                          ),
-                        ]),
+                          textAlign: TextAlign.left,
+                        ),
                       ),
+                      SizedBox(
+                        height: 12.0,
+                      ),
+                    ]),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.blue
+                      )
                     ),
-                  ]),
+                    child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+                      Padding(
+                        padding: EdgeInsets.all(7.0),
+                        child: StepProgressIndicator(
+                          totalSteps: 3,
+                          currentStep: UserWarningLevel(user),
+                          size: 30,
+                          selectedColor: Colors.orange,
+                          unselectedColor: Colors.lightGreen, //[200],
+                          customStep: (index, colorTemp, _) => colorTemp == Colors.orange
+                              ? Container(
+                                  color: colorTemp,
+                                  child: Tooltip(
+                                    message: warningLevelString(index),
+                                    child: Icon(
+                                      Icons.warning,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                )
+                              : Container(
+                                  color: colorTemp,
+                                  child: Tooltip(
+                                    message: warningLevelString(index),
+                                    child: Icon(
+                                      Icons.remove,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12.0,
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: EdgeInsets.all(7.0),
+                          child: Row(children: [
+                            Icon(
+                              Icons.update,
+                              //color: Colors.black,
+                              size: 18.0,
+                              semanticLabel: 'Letzte Änderung',
+                            ),
+                            Tooltip(
+                              message: "letzte Änderung: " + DateFormat('dd.MM.yyyy Hm').format(DateTime.fromMillisecondsSinceEpoch(user.lastModified * 1000)),
+                              child: Text(
+                                " " + timeago.format(DateTime.fromMillisecondsSinceEpoch(user.lastModified * 1000), locale: 'de'), // source: https://stackoverflow.com/a/50632411 - users: Mahesh Jamdade & Alex Haslam - License: CC BY-SA 4.0
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                textAlign: TextAlign.left,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            Icon(
+                              Icons.update,
+                              //color: Colors.black,
+                              size: 18.0,
+                              semanticLabel: 'effective',
+                            ),
+                            Tooltip(
+                              message: "effective -> " + DateFormat('dd.MM.yyyy Hm').format(DateTime.fromMillisecondsSinceEpoch(user.effective * 1000)),
+                              child: Text(
+                                " " + timeago.format(DateTime.fromMillisecondsSinceEpoch(user.effective * 1000), locale: 'de'), // source: https://stackoverflow.com/a/50632411 - users: Mahesh Jamdade & Alex Haslam - License: CC BY-SA 4.0
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                textAlign: TextAlign.left,
+                              ),
+                            ),
+                          ]),
+                        ),
+                      ),
+                    ]),
+                  ),
                 ),
               ]),
             ),
             SliverList(
               delegate: SliverChildListDelegate([
-                SizedBox(
-                  height: 12.0,
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: HtmlWidget(
-                    user.content,
-                    onErrorBuilder: (context, element, error) => Text('$element error: $error'),
-                    onLoadingBuilder: (context, element, loadingProgress) => CircularProgressIndicator(),
-                    webView: false,
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.blue
+                      )
+                    ),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: HtmlWidget(
+                            user.content,
+                            onErrorBuilder: (context, element, error) => Text('$element error: $error'),
+                            onLoadingBuilder: (context, element, loadingProgress) => CircularProgressIndicator(),
+                            webView: false,
+                          ),
+                      ),
+                    ),
                   ),
                 ),
               ]),
